9bcf5955d31fc0d3e5921613e57897a64e9f337f
RFID-NC <stoyann.noumea@outlook.com>
2024-03-05 19:17:39 +1100

    artcle comment

--- frontend/src/screens/public/homeScreen/HomeScreen.jsx
+++ frontend/src/screens/public/homeScreen/HomeScreen.jsx
@@ -10,7 +10,7 @@ const HomeScreen = () => {
       <HomeHero />
       <IconsSection/>
       <HomeAbout/>
-      <HomeArticles />
+      {/* <HomeArticles /> */}
       <HomeContacts />
     </>
   )
