c2a6c98d50de25ef9d59087c44c25c5acc86edef
RFID-NC <stoyann.noumea@outlook.com>
2024-03-08 15:42:37 +1100

    test

--- frontend/src/screens/NotFound.jsx
+++ frontend/src/screens/NotFound.jsx
@@ -5,8 +5,8 @@ const NotFound = () => {
   return (
     <div className='page-container'>
       <div className="notFound-container">
-        <h1>OUPS !!!!!!!</h1>
-        <h2>404 </h2>
+        <h1>OUPS</h1>
+        <h2>404 JJKJKKJJJ</h2>
         <p>Désolée, la page que vous demandez n'est pas trouvée.</p>
         <Link className='btn btn-block' to={"/"}>Retour à l'accueil</Link>
       </div>
