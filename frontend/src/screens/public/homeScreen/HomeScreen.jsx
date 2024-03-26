e76481da98c59aa6161e418d4e91338940dbceac
RFID-NC <stoyann.noumea@outlook.com>
2024-03-26 11:59:12 +1100

    add new style

--- frontend/src/screens/public/homeScreen/HomeScreen.jsx
+++ frontend/src/screens/public/homeScreen/HomeScreen.jsx
@@ -13,7 +13,7 @@ const HomeScreen = () => {
       <HomeHero />
       {/* <HomeCategories/> */}
       
-      <div className="products-flex">
+      <div className="flex-item">
 
       <Products/>
       </div>
