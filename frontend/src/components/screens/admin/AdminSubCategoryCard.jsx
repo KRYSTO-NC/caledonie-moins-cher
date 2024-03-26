d21d63ec208f8586ec86f02b9969eaac8c5aaf96
RFID-NC <stoyann.noumea@outlook.com>
2024-03-27 10:23:12 +1100

    handle delete subcategory

--- frontend/src/components/screens/admin/AdminSubCategoryCard.jsx
+++ frontend/src/components/screens/admin/AdminSubCategoryCard.jsx
@@ -1,22 +1,41 @@
-import { Link } from 'react-router-dom'
-import './adminComponents.css'
-import Modal from '../../shared/modal/Modal'
-import { FaEdit } from 'react-icons/fa'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './adminComponents.css';
+import Modal from '../../shared/modal/Modal';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { useDeleteSubCategoryMutation } from '../../../slices/subCategoriesApiSlice';
 
 const AdminSubCategoryCard = ({ subcategory }) => {
-  console.log(subcategory)
+  const [deleteSubCategory, { isLoading: deletingSubCategory }] = useDeleteSubCategoryMutation();
+
+  const handleDelete = async () => {
+    try {
+      await deleteSubCategory(subcategory._id);
+      window.location.reload();
+     
+      toast.success('Sous-catégorie supprimée avec succès');
+    } catch (error) {
+      console.error('Erreur lors de la suppression de la sous-catégorie :', error);
+      toast.error('Erreur lors de la suppression de la sous-catégorie');
+    }
+  };
 
   return (
+    <div      className="admin-sub-category-card">
     <Link
       to={`/admin/sub-category/${subcategory._id}`}
-      className="admin-sub-category-card"
-    >
+  
+      >
       <p>{subcategory.name}</p>
-      <Modal modalBtn={<FaEdit />}>
-
-      </Modal>
+  
+     
     </Link>
-  )
-}
+      <button className="btn" onClick={handleDelete} disabled={deletingSubCategory}>
+        <FaTrash />
+      </button>
+      </div>
+  );
+};
 
-export default AdminSubCategoryCard
+export default AdminSubCategoryCard;
