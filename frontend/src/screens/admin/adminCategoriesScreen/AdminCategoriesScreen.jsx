6d7dba4b4a28835e6e9516a9264656bf3d6dca0f
RFID-NC <stoyann.noumea@outlook.com>
2024-03-28 15:38:13 +1100

    add categorie tables

--- frontend/src/screens/admin/adminCategoriesScreen/AdminCategoriesScreen.jsx
+++ frontend/src/screens/admin/adminCategoriesScreen/AdminCategoriesScreen.jsx
@@ -1,67 +1,67 @@
-import './adminCategories.css'
-import { Link } from 'react-router-dom'
+import './adminCategories.css';
+import { Link } from 'react-router-dom';
 import {
   useGetCategoriesQuery,
   useCreateCategoryMutation,
   useUpdateCategoryMutation,
-} from '../../../slices/categoriesSlice'
-import { FaEdit, FaPlus } from 'react-icons/fa'
-import { toast } from 'react-toastify'
-import Modal from '../../../components/shared/modal/Modal'
-import { useState } from 'react'
+} from '../../../slices/categoriesSlice';
+import { FaEdit, FaEye, FaPlus } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import Modal from '../../../components/shared/modal/Modal';
+import { useState } from 'react';
 
 const AdminCategoriesScreen = () => {
-  const { data, isLoading, error, refetch } = useGetCategoriesQuery()
+  const { data, isLoading, error, refetch } = useGetCategoriesQuery();
 
   const [
     createCategory,
     { isLoading: loadingCreate },
-  ] = useCreateCategoryMutation()
+  ] = useCreateCategoryMutation();
 
   const createCategoryHandler = async () => {
     if (window.confirm('Êtes-vous sûr de vouloir ajouter une catégorie ?')) {
       try {
-        await createCategory()
-        refetch()
+        await createCategory();
+        refetch();
       } catch (err) {
-        toast.error(err?.data?.message || err.error)
+        toast.error(err?.data?.message || err.error);
       }
     }
-  }
+  };
+
   const updateCategoryHandler = async (categoryId) => {
     try {
       await updateCategory({
         categoryId,
         name: newCategoryName,
-      })
-      console.log(updateCategory)
-      setEditingCategoryId(null)
-      setNewCategoryName('')
-      refetch()
+      });
+      console.log(updateCategory);
+      setEditingCategoryId(null);
+      setNewCategoryName('');
+      refetch();
     } catch (err) {
-      toast.error(err?.data?.message || err.error)
+      toast.error(err?.data?.message || err.error);
     }
-  }
+  };
 
-  const [selectedCategoryId, setSelectedCategoryId] = useState(null)
+  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
 
   const [
     updateCategory,
     { isLoading: loadingUpdate },
-  ] = useUpdateCategoryMutation()
+  ] = useUpdateCategoryMutation();
 
-  const [editingCategoryId, setEditingCategoryId] = useState(null)
-  const [newCategoryName, setNewCategoryName] = useState('')
+  const [editingCategoryId, setEditingCategoryId] = useState(null);
+  const [newCategoryName, setNewCategoryName] = useState('');
 
-  console.log('====================================')
-  console.log(data)
-  console.log('====================================')
+  console.log('====================================');
+  console.log(data);
+  console.log('====================================');
 
   return (
     <div className="page-container">
       <section className="heading">
         <h1>Categories</h1>
-
         <button className="btn" onClick={createCategoryHandler}>
           <FaPlus /> Ajouter une catégorie
         </button>
@@ -71,48 +71,65 @@ const AdminCategoriesScreen = () => {
       {error && <p>Error: {error.message}</p>}
       {data && (
         <div className="admin-categories-container">
-          {data.map((category) => (
-            <>
-              <Link
-                className="btn"
-                to={`/admin/category/${category._id}`}
-                key={category._id}
-              >
-                <h3>{category.name}</h3>
-                <Link>
-                  <Modal modalBtn={<FaEdit />}>
-                    <h1>Modifier la catégorie</h1>
-                    <div className="form">
-                      <form
-                        onSubmit={(e) => {
-                          e.preventDefault()
-                          updateCategoryHandler(category._id)
-                        }}
-                      >
-                        <div className="form-group">
-                          <label htmlFor="name">Nom</label>
-                          <input
-                            type="text"
-                            id="name"
-                            placeholder="Entrez le nom de la catégorie"
-                            value={newCategoryName}
-                            onChange={(e) => setNewCategoryName(e.target.value)}
-                          />
-                        </div>
-                        <button className="btn" type="submit">
-                          Modifier
-                        </button>
-                      </form>
-                    </div>
-                  </Modal>
-                </Link>
-              </Link>
-            </>
-          ))}
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Voir</th>
+                <th>Editer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((category) => (
+                <tr key={category._id}>
+                  <td>{category._id}</td>
+                  <td>{category.name}</td>
+                  <td>
+                    <Link to={`/admin/category/${category._id}`}>
+                      <FaEye />
+                    </Link>
+
+
+
+                    </td>
+                    <td>
+                    <Modal modalBtn={<FaEdit />}>
+                      <h1>Modifier la catégorie</h1>
+                      <div className="form">
+                        <form
+                          onSubmit={(e) => {
+                            e.preventDefault();
+                            updateCategoryHandler(category._id);
+                          }}
+                        >
+                          <div className="form-group">
+                            <label htmlFor="name">Nom</label>
+                            <input
+                              type="text"
+                              id="name"
+                              placeholder="Entrez le nom de la catégorie"
+                              value={newCategoryName}
+                              onChange={(e) =>
+                                setNewCategoryName(e.target.value)
+                              }
+                            />
+                          </div>
+                          <button className="btn" type="submit">
+                            Modifier
+                          </button>
+                        </form>
+                      </div>
+                    </Modal>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default AdminCategoriesScreen
+export default AdminCategoriesScreen;
